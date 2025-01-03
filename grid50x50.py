839cf4edbf9dc171815637c5a08e78adda0f3bca
Kacper Zawislanski <zawislanski2@gmail.com>
2025-01-03 12:04:33 +0100

    Udalo sie zrobic by po kliknieciu zmieniala kolor, ale nie da sie go ponownie zmmienic

--- grid50x50.py
+++ grid50x50.py
@@ -5,23 +5,25 @@ files = []
 btn=[]
 status=[]
 def btn_color(status):
-    if status==0:
-        return("black")
     if status==1:
+        return("black")
+    if status==0:
         return("white")
 def click_update(status,btn):
+    return(lambda: click_update1(status,btn))
+def click_update1(status,btn):
     status=abs(status-1)
-    if status==0:
-        btn.config(bg="black")
     if status==1:
+        btn.config(bg="black")
+    if status==0:
         btn.config(bg="white")
-
 for i in range(2500):
     files.append("button"+str(1))
 for i in range(len(files)):
     status.append(int(0))
-    btn.append(Button(frame,text="k", font=("Arial",3),width=2,height=2))
-    btn[i].grid(row=int((i)//50),column=int((i-((i//50))*50)),sticky="we")
+    btn.append(Button(frame,text="k", font=("Arial",3),width=2,height=2,repeatdelay=100,repeatinterval=100))
+    btn[i].grid(row=int(i//50),column=int(i%50),sticky="we")
 for i in range(len(files)):
-     btn[i].config(bg=btn_color(status[i]), command=lambda: click_update(status[i],btn[i]))
+    
+    btn[i].config(bg=btn_color(status[i]), command=click_update(status[i],btn[i]))
 window.mainloop()
