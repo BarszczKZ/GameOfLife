e70ba2110ba002daaf150ffbd734caa103ac7330
Kacper Zawislanski <zawislanski2@gmail.com>
2025-01-08 00:01:33 +0100

    drobne poprawki

--- prototyp7.py
+++ prototyp7.py
@@ -4,7 +4,6 @@
 
 from tkinter import *
 import time
-import sys
 window = Tk()
 window.title("Game of Life")
 window.geometry("700x910")
@@ -14,19 +13,19 @@ menu_frame = Frame(window)
 menu_frame.grid(sticky="w")
 size = 2500 #jest to rozmiar siatki, i lepiej tege nie zmieniac bo wszystko sie rozjedzie
 btn=[] 
-status = [0] * size  #tworzy początkowa liste statusu (każda komórka na starcie jest martwa)
+btn_status = [0] * size  #tworzy początkowa liste statusu przycisków "komórek" (każda komórka na starcie jest martwa "0", żywa ma status "t"), w zależności od statusu zmienia sie kolor komórki 
 menu_status=0 
 orientation=3 #orientacja mrówki, można pomyslec nad opcją wyboru orientacji przed startem symulacji
-a_status= [0] * size #tworzy liste statusu na której będzie zapisane w której komórce znajduje sie mrówka
+ant_status= [0] * size #tworzy liste na której zapisane jest  na której będzie zapisane w której komórce znajduje sie mrówka
 steps=1000  #ilosc kroków w symulacji życia, ale da sie tez ja zrobic dla nieskonczonej ilosci kroków
-def simulation():   #to jest funkcja symulacji mrówki
-    global menu_status, a_status, size
-    while 1>0:
-        time.sleep(1)
+def ant_simulation():   #to jest funkcja symulacji mrówki
+    global menu_status, ant_status, size
+    while True:
+        time.sleep(0.01)
         window.update()
         for i in range(size):
-            if a_status[i]=="m":
-                movement(i)
+            if ant_status[i]=="m":
+                ant_movement(i)
                 break
         if menu_status==0:
             break
@@ -34,32 +33,32 @@ def simulation():   #to jest funkcja symulacji mrówki
 
 def click_update(btn,i):   # ta funkcja jest od przycisków na siatce
     def in_func(btn,i):
-        global status, menu_status, a_status
+        global btn_status, menu_status, ant_status
         if menu_status==1:
-            a_status[i]="m"
+            ant_status[i]="m"
             menu_status=2
             menu.config(text="Start Simulation")
-        if status[i]==0 and menu_status==0:
-            status[i]=1
-        elif status[i]==1 and menu_status==0:
-            status[i]=0
-        if status[i]==1:
+        if btn_status[i]==0 and menu_status==0:
+            btn_status[i]=1
+        elif btn_status[i]==1 and menu_status==0:
+            btn_status[i]=0
+        if btn_status[i]==1:
             btn.config(bg="black")
-        if status[i]==0:
+        if btn_status[i]==0:
             btn.config(bg="white")
-        if a_status[i]=="m":
+        if ant_status[i]=="m":
             btn.config(bg="red")
     return(lambda: in_func(btn,i))
 
 def menu_fun():  #ta funkcja jest od przycisku menu, ale na razie obsługuje tylko symulacje mrówki
-    global menu_status, btn, status, a_status, orientation
+    global menu_status, btn, btn_status, ant_status, orientation
     if menu_status==0:
         menu_status=1
         print(menu_status)
     if menu_status==3:
         for i in range(size):
-            a_status[i]=0
-            status[i]=0
+            ant_status[i]=0
+            btn_status[i]=0
             btn[i].config(state=NORMAL,bg="white")
         menu.config(text="Place Ant")
         menu_status=0
@@ -69,32 +68,32 @@ def menu_fun():  #ta funkcja jest od przycisku menu, ale na razie obsługuje tyl
             btn[i].config(state=DISABLED)
         menu.config(text="Restart Simulation")
         menu_status=3
-        simulation()
+        ant_simulation()
 
-def which_neighbor(i,ant_neighbors):
-    global orientation, status, a_status, btn
-    if status[i]==0:
+def which_neighbor(i,ant_neighbors): #przesuwa mrówke do właściwego sąsiada i obraca ją
+    global orientation, btn_status, ant_status, btn
+    if btn_status[i]==0:
         orientation=(orientation+1)%4
-        a_status[i]=0
-        a_status[i+ant_neighbors[orientation]]="m"
+        ant_status[i]=0
+        ant_status[i+ant_neighbors[orientation]]="m"
         btn[i+ant_neighbors[orientation]].config(bg="red")
         btn[i].config(bg="black")
-        status[i]=1
+        btn_status[i]=1
         
-    elif status[i]==1:
+    elif btn_status[i]==1:
         orientation-=1
         if orientation<0:
             orientation=3
-        a_status[i]=0
-        a_status[i+ant_neighbors[orientation]]="m"
+        ant_status[i]=0
+        ant_status[i+ant_neighbors[orientation]]="m"
         btn[i+ant_neighbors[orientation]].config(bg="red")
         btn[i].config(bg="white")
-        status[i]=0
+        btn_status[i]=0
         
     
-def movement(i):
+def ant_movement(i):
     
-    if i == 2499:
+    if i == 2499:  #poniższy kod sprawdza jakich sąsiadów posiada dana komórka
         ant_neighbors = [-49, -2450, -1, -50]
     elif i == 2450:
         ant_neighbors = [+1, -2450, +49, -50]
@@ -102,42 +101,34 @@ def movement(i):
         ant_neighbors = [+1, +50, +49, +2450]
     elif i==49:
         ant_neighbors = [-49, +50, -1, +2450]
-    elif (i + 1) % 50 == 0 and i != 49 and i != 2499:
+    elif (i + 1) % 50 == 0:
         ant_neighbors = [-49, +50, -1, -50]
-    elif i % 50 == 0 and i != 0 and i != 2450:
+    elif i % 50 == 0 :
         ant_neighbors = [+1, +50, +49, -50]
-    elif i < 50 and i != 0 and i != 49:
+    elif i < 50 :
         ant_neighbors = [+1, +50, -1, +2450]
-    elif i > 2450 and i != 2450 and i != 2499:
+    elif i > 2450 :
         ant_neighbors = [+1, -2450, -1, -50]
-    elif i < 2450 and i >= 50 and (i + 1) % 50 != 0 and i % 50 != 0:
+    else:
         ant_neighbors = [+1, +50, -1, -50]
     which_neighbor(i, ant_neighbors)
 
-
-
-def color_change(i):    # wsm nie wiem czy gdzies tego użylem, jesli nie to mozna wyjebac
-    global btn, status
-    if status[i]==0:
-        btn[i].config(bg="white")
-    elif status[i]==1:
-        btn[i].config(bg="white")
-def count_neighbors(i, neighbors): # Liczy sąsiadów zadanej komórki i
-    global status
+def count_neighbors(i, neighbors): # Liczy sąsiadów zadanej komórki 
+    global btn_status
     k=0
     for n in range(8):
         w=0
         w=i+neighbors[n]
-        if status[w]==1:
+        if btn_status[w]==1:
             k+=1
     return k
-def gl_simulation():
-    global status, btn,  menu_status
+def gl_simulation(): #symulacja "gry w życie"
+    global btn_status, btn,  menu_status
     while True:
-        status_copy = [0] *2500
+        btn_status_copy = [0] *2500
         for i in range(2500):
             k = 0
-            if i == 2499:
+            if i == 2499: #poniższy kod sprawdza jakich sąsiadów posiada dana komórka
                 neighbors = [-2450, -2499, -2451, -1, -51, -50, -99, -49]
             elif i == 2450:
                 neighbors = [-2450, -2401, -2449, +1, -1, -49, -50, +49]
@@ -145,31 +136,31 @@ def gl_simulation():
                 neighbors = [+2451, +2450, +2499, +1, +99, +50, +49, +51]
             elif i==49:
                 neighbors = [+2450, +2401, +2449, +1, -1, -49, +50, +49]
-            elif (i + 1) % 50 == 0 and i != 49 and i != 2499:
+            elif (i + 1) % 50 == 0:
                 neighbors = [-99, -49, +1, +50, +49, -51, -1, -50]
-            elif i % 50 == 0 and i != 0 and i != 2450:
+            elif i % 50 == 0:
                 neighbors = [+99, +49, -1, -50, -49, +1, +51, +50]
-            elif i < 50 and i != 0 and i != 49:
+            elif i < 50:
                 neighbors = [+1, -1, +49, +50, +51, +2451, +2449, +2450]
-            elif i > 2450 and i != 2450 and i != 2499:
+            elif i > 2450:
                 neighbors = [-2451, -2449, -2450, +1, -1, -50, -51, -49]
-            elif i < 2450 and i >= 50 and (i + 1) % 50 != 0 and i % 50 != 0:
+            else:
                 neighbors = [+1, -1, +50, -50, -51, +51, -49, +49]
 
             k = count_neighbors(i, neighbors)
 
-            if status[i] == 0 and k == 3:
-                status_copy[i] = 1
+            if btn_status[i] == 0 and k == 3:
+                btn_status_copy[i] = 1
                 btn[i].config(bg="black")
-            elif status[i] == 1:
+            elif btn_status[i] == 1:
                 if k != 2 and k != 3:
-                    status_copy[i] = 0
+                    btn_status_copy[i] = 0
                     btn[i].config(bg="white")
                 else:
-                    status_copy[i] = 1
+                    btn_status_copy[i] = 1
 
-        status.clear()
-        status.extend(status_copy)
+        btn_status.clear()
+        btn_status.extend(btn_status_copy)
         window.update()
 
     
